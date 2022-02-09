a755ea2dedff68b7a1e254aa5357a9b42ac201b2
Shawon <vdr_sakando@parata.com>
2022-02-09 10:24:32 +0000

    First Commit

--- src/test/java/steps/purgeOrphanTestStep.java
+++ src/test/java/steps/purgeOrphanTestStep.java
@@ -17,17 +17,23 @@ import java.util.Scanner;
 public class purgeOrphanTestStep {
     private Connection connection;
     private String connectionStatus;
-
+    private ArrayList<String[]> statusDetails = new ArrayList<>();
+    private Integer checked = 0;
+    private int checkedBy = 0;
+    private Integer XMLGenerated = 0;
+    private Integer CVSAcknowlogement = 0;
+    private Integer UpdateStatusID = 0;
+    private String orderId = "";
+    private Integer BagNumber = 0;
     @Given("Sql server connection")
     public void sqlServerConnection() {
-        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=BacktalkDB;user=shawon;password=sa1234";
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=BacktalkDB;user=shawon;password=shawon963";
         try {
             // Load SQL Server JDBC driver and establish connection.
             System.out.print("Connecting to SQL Server ... ");
             connection = DriverManager.getConnection(connectionUrl);
             System.out.println("Connected");
             connectionStatus = "Success";
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             connectionStatus = "Error";
@@ -50,35 +56,22 @@ public class purgeOrphanTestStep {
         else {
             isTableExists = false;
         }
-
         Assert.assertTrue(isTableExists);
     }
 
     @And("I should not get orphan data")
     public void iShouldNotGetOrphanData() throws SQLException {
-        String sql = " select count(*) from [BacktalkDB].[dbo].[StatusUpdate] where DATEDIFF(DAY, CheckedDateTime, GETDATE()) > 14";
+        String sql = " select count(*) from [BacktalkDB].[dbo].[StatusUpdate] where DATEDIFF(DAY, CheckedDateTime, GETDATE()) < 14";
         PreparedStatement stmt = connection.prepareStatement(sql);
         ResultSet rs = stmt.executeQuery();
         rs.next();
         int count = rs.getInt(1);
-        Assert.assertEquals("Number of records", 0, count);
+        Assert.assertEquals("Number of records in StatusUpDate Table", 7, count);
     }
 
-    @And("I should read data from {string} text file")
-    public void iShouldReadDataFromTextFile(String fileName) {
-//        String content = null;
-//        File file = new File(fileName); // For example, foo.txt
-//        FileReader reader = null;
-//        try {
-//            reader = new FileReader(file);
-//            char[] chars = new char[(int) file.length()];
-//            reader.read(chars);
-//
-//            content = new String(chars);
-//            reader.close();
+    @Then("I should read data from {string} batch file")
+    public void iShouldReadDataFromBatchFile(String fileName) {
         File file = new File(fileName);
-        ArrayList<String[]> arrayOfPeople = new ArrayList<>();
-
         try {
             Scanner input = new Scanner(file);
             int i=0;
@@ -86,22 +79,72 @@ public class purgeOrphanTestStep {
                 input.nextLine();                         //do this to skip the first line (header)while (input.hasNext()) {
                 String num = input.nextLine();
                 String[] personData = num.split(","); //returns the array of strings computed by splitting this string around matches of the given delimiter
-                arrayOfPeople.add(personData);
-
-                for(int j=0; j<arrayOfPeople.get(i).length; j++){
-                    System.out.print("Column :"+j+"= " +arrayOfPeople.get(i)[j]+ " ");
-                }
-
-              System.out.print("\n===============\n");
+                statusDetails.add(personData);
                 i++;
             }
-//            for(int i=0; i<arrayOfPeople.size(); i++){
-//                System.out.println(arrayOfPeople.get(i));
-//            }
-
-
+            System.out.println(statusDetails);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @And("I should check data with {string} table")
+    public void iShouldCheckWithStatusUpdateTable(String tableName) throws SQLException {
+        boolean status = false;
+        String sql = "select UpdateStatusID, Checked, XMLGenerated, CVSAcknowlogement from "+tableName+" where OrderID = '"+ orderId+ "'and BagNumber ="+BagNumber+" and CheckedBy = "+checkedBy;
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            UpdateStatusID =Integer.parseInt(rs.getString(1));
+            checked =Integer.parseInt(rs.getString(2));
+            XMLGenerated = Integer.parseInt(rs.getString(3));
+            CVSAcknowlogement = Integer.parseInt(rs.getString(4));
+            break;
+        }
+        if((checked == 1) && (XMLGenerated == 1) && (CVSAcknowlogement == 1)){
+            status = true;
+        }
+        else {
+            status = false;
+        }
+        Assert.assertTrue("Checked Data With StatusUpDate Table",status);
+    }
+
+    @And("I should check {string} table data")
+    public void iShouldCheckTableData(String tableName) throws SQLException {
+        String sql = "select count(*) from "+tableName+" where UpdateStatusID = "+ UpdateStatusID + " and Status = 'true'";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+        Assert.assertEquals("Checked Data with PMSMessageLog Table",1, count);
+    }
+
+    @Given("Sql server connection with inspectRxDB")
+    public void sqlServerConnectionWithinspectRxDB() {
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=dispensercheck;user=shawon;password=shawon963";
+        try {
+            // Load SQL Server JDBC driver and establish connection.
+            System.out.print("Connecting to SQL Server ... ");
+            connection = DriverManager.getConnection(connectionUrl);
+            System.out.println("Connected");
+            connectionStatus = "Success";
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            connectionStatus = "Error";
+        }
+    }
+
+    @Then("I should get data {string} table with orderId and bagNumber")
+    public void iShouldGetDataWithOrderIdAndBagNumber(String tableName) throws SQLException{
+        orderId = statusDetails.get(0)[0].substring(1,8);
+        BagNumber = Integer.parseInt(statusDetails.get(0)[20].substring(1,2));
+        String sql = "select checked_by from "+tableName+" where batch = "+ orderId + " and bag = "+BagNumber+" and checked = 1 and is_result = 1";
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        checkedBy = rs.getInt(1);
+        Assert.assertEquals("Checked Data With b_batch_bag Table",100,checkedBy);
     }
 }
--- src/test/resources/features/purgeOrphan.feature
+++ src/test/resources/features/purgeOrphan.feature
@@ -5,11 +5,18 @@ Feature: Purged Orphan Data Test
     And Table "StatusUpdate" should exists
     And I should not get orphan data
 
-  Scenario: Data Read From Text file
-   #  Given Sql server connection
-   # Then I should get connection status "Success"
-   # And Table "StatusUpdate" should exists
-    And I should read data from "c:/Innovation/Input/Batch0200232.txt" text file
+  Scenario: Data Read From Text file And Check With InspectRxDB and BacktalkDB
+    Given I should read data from "c:/Innovation/Input/Batch8069845.txt" batch file
+    Given Sql server connection with inspectRxDB
+    Then I should get connection status "Success"
+    And Table "b_batch_bag" should exists
+    Then I should get data "b_batch_bag" table with orderId and bagNumber
+    Given Sql server connection
+    Then I should get connection status "Success"
+    And Table "StatusUpdate" should exists
+    And I should check data with "StatusUpdate" table
+    And Table "PMSMessageLog" should exists
+    And I should check "PMSMessageLog" table data
 
 
 
